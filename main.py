3f3e49978124c5d610751a8678bd7c65804420b8
Ashshoffner1 <amshoffner936@access.alamancecc.edu>
2024-12-16 13:41:32 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Note:
+# --> This is a separate project to provide those who turned in Project 1 with extra credit.
+# --> Those that did not turn in Projet 1 is an opportunity to get partial credit for Project 1.
+
+
+# The instructions are the same as Project 1. Please follow those as part of:
+# -- a) data wrangling and
+# -- b) data exploration / visualiztion
+
+# Good luck!
+# --> Create a new branch and push to the git repository
